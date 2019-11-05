421a52fe469735ffe7bef1ec0cfaa8cfc6c97dfe
Colin <ledbetter.colin.m@gmail.com>
2019-11-05 01:33:03 -0500

    Expanding Budget to be more of a container and less of an action class

--- app/src/main/java/com/accutech/budgets/model/Budget.java
+++ app/src/main/java/com/accutech/budgets/model/Budget.java
@@ -1,9 +1,70 @@
 package com.accutech.budgets.model;
 
+import java.util.Map;
+
+import static com.accutech.budgets.model.BudgetCategory.EDUCATION;
+import static com.accutech.budgets.model.BudgetCategory.ENTERTAINMENT;
+import static com.accutech.budgets.model.BudgetCategory.GROCERIES;
+import static com.accutech.budgets.model.BudgetCategory.HEALTH;
+import static com.accutech.budgets.model.BudgetCategory.HOUSING;
+import static com.accutech.budgets.model.BudgetCategory.KIDS;
+import static com.accutech.budgets.model.BudgetCategory.MISCELLANEOUS;
+import static com.accutech.budgets.model.BudgetCategory.PETS;
+import static com.accutech.budgets.model.BudgetCategory.SAVINGS;
+import static com.accutech.budgets.model.BudgetCategory.TRANSPORTATION;
+import static com.accutech.budgets.model.BudgetCategory.UTILITIES;
+
 public class Budget {
 
-    private Budget(int age, int monthlyIncome, HousingOwnership housingOwnership, int housingPayment, int debt, int savings) {
-        // TODO implement budget from minimum info required
+    private Double monthlyIncome = 0.0;
+    private Map<BudgetCategory, Double> recommendedSpending;
+    private Map<BudgetCategory, Double> actualSpending;
+
+    private Budget() {
+        populateRecommendedSpending();
+        populateActualSpending();
+    }
+
+    private void populateRecommendedSpending() {
+        recommendedSpending.put(HOUSING, 0.0);
+        recommendedSpending.put(UTILITIES, 0.0);
+        recommendedSpending.put(GROCERIES, 0.0);
+        recommendedSpending.put(SAVINGS, 0.0);
+        recommendedSpending.put(HEALTH, 0.0);
+        recommendedSpending.put(TRANSPORTATION, 0.0);
+        recommendedSpending.put(EDUCATION, 0.0);
+        recommendedSpending.put(ENTERTAINMENT, 0.0);
+        recommendedSpending.put(KIDS, 0.0);
+        recommendedSpending.put(PETS, 0.0);
+        recommendedSpending.put(MISCELLANEOUS, 0.0);
+    }
+
+    private void populateActualSpending() {
+        actualSpending.put(HOUSING, 0.0);
+        actualSpending.put(UTILITIES, 0.0);
+        actualSpending.put(GROCERIES, 0.0);
+        actualSpending.put(SAVINGS, 0.0);
+        actualSpending.put(HEALTH, 0.0);
+        actualSpending.put(TRANSPORTATION, 0.0);
+        actualSpending.put(EDUCATION, 0.0);
+        actualSpending.put(ENTERTAINMENT, 0.0);
+        actualSpending.put(KIDS, 0.0);
+        actualSpending.put(PETS, 0.0);
+        actualSpending.put(MISCELLANEOUS, 0.0);
+    }
+
+    /**
+     *
+     * @param category
+     * @param amount
+     * @return
+     */
+    public Double addSpending(BudgetCategory category, Double amount) {
+        Double currentSpending = actualSpending.get(category);
+        currentSpending += amount;
+        actualSpending.remove(category);
+        actualSpending.put(category, currentSpending);
+        return currentSpending;
     }
 
 }
