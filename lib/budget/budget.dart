14741c169f24c9f28974ac34168c71ac5ce7b661
Colin <ledbetter.colin.m@gmail.com>
2019-11-24 02:14:33 -0500

    Beginning to move items from android into flutter/dart, finished budget_category.dart and started budget.dart

---
+++ lib/budget/budget.dart
@@ -0,0 +1,27 @@
+import 'dart:ffi';
+
+import 'budget_category.dart';
+
+class Budget {
+
+  Map<BudgetCategory, double> allottedSpending, actualSpending;
+  double monthlyIncome;
+
+  Budget() {
+    populateMaps();
+  }
+
+  void populateMaps() {
+    allottedSpending = new Map();
+    actualSpending = new Map();
+    for (BudgetCategory category in BudgetCategory.values) {
+      allottedSpending[category] = 0.0;
+      actualSpending[category] = 0.0;
+    }
+  }
+
+  double zero() {
+    return 0.0;
+  }
+
+}
---
+++ lib/budget/budget_category.dart
@@ -0,0 +1,15 @@
+enum BudgetCategory {
+
+  housing,
+  utilities,
+  groceries,
+  savings,
+  health,
+  transportation,
+  education,
+  entertainment,
+  kids,
+  pets,
+  miscellaneous
+
+}
