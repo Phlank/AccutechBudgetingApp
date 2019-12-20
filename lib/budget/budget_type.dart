c49ae5f151e120ecc26603b986f9982805e25fa2
Jawad Al-Mamoon <jaalmamoon@bsu.edu>
2019-12-20 13:51:11 +0600

    Added Budgeting brackets for different income percentages. Developed skeleton for test class.

---
+++ lib/budget/budget_type.dart
@@ -0,0 +1,4 @@
+enum BudgetType{
+  savingGrowth,
+  savingDepletion
+}
---
+++ test/budget_tests.dart
@@ -0,0 +1,14 @@
+import 'package:budgetflow/budget/budget_writer.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main(){
+  //TODO develop tests for categorize budget, setbudgetgrowthratio, setbudgetdepletionratio
+  group("Budget Functions", (){
+    test("Categorize functions properly", () {
+      budgetWriter budgetwriter = new budgetWriter();
+    });
+    test("Budget a Normal Ratio", (){
+      budgetWriter budgetwriter = new budgetWriter();
+    });
+  });
+}
