af50c498751022b7872a3b9dba45094918466c30
Colin <ledbetter.colin.m@gmail.com>
2020-01-03 00:43:14 -0500

    Added some comments to code and reformatted all files in lib/crypt

--- lib/crypt/password.dart
+++ lib/crypt/password.dart
@@ -1,7 +1,9 @@
 class Password {
-
   bool verify(String secret, String salt) {}
 
+  // This needs to exist so we have something to call verify() on.
+  // Without this function, we have to work with the bare bone libraries, and
+  // that's just nasty.
   Password hashOnlyPassword(String hash) {}
 
   String getHash() {}
@@ -10,14 +12,16 @@ class Password {
 
   String getSalt() {}
 
+  // A helper function for some implementations of passwords which may need
+  // padding to certain integers.
   String padTo(int n, String input) {
-  	while (input.length < n) {
-  		// Pad once then double
-	    input = input + " " + input;
+    while (input.length < n) {
+      // Pad once then double
+      input = input + " " + input;
     }
     while (input.length > n) {
-  		// Take characters off of front until n
-    	input = input.replaceFirst(input.substring(0, 1), '');
+      // Take characters off of front until n
+      input = input.replaceFirst(input.substring(0, 1), '');
     }
     return input;
   }
