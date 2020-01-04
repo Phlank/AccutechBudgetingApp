8a06458d82e5d1f5b625fde0c34b3d969d652d8e
Colin <ledbetter.colin.m@gmail.com>
2020-01-04 17:37:47 -0500

    Expanded a comment in password.dart for more explanation of implementation and use

--- lib/crypt/password.dart
+++ lib/crypt/password.dart
@@ -3,7 +3,8 @@ class Password {
 
   // This needs to exist so we have something to call verify() on.
   // Without this function, we have to work with the bare bone libraries, and
-  // that's just nasty.
+  // that's just nasty. When verify() is called, it should make all other
+  // functions usable because it takes secret and salt as params.
   Password hashOnlyPassword(String hash) {}
 
   String getHash() {}
