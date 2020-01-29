57511d05418250d966ad0f2d0f5fdc2925d9e703
CaKellum <cakellum@bsu.edu>
2020-01-29 12:43:10 -0500

    added semi colon

--- lib/view/sidebar/user_catagory_displays.dart
+++ lib/view/sidebar/user_catagory_displays.dart
@@ -125,7 +125,7 @@ class GeneralSliderCategory{// todo figure out why sliders don't slide and make 
 			activeColor: Colors.lightGreen,
 			value:userController.getBudget().allotted[userController.categoryMap[category]],
 			onChanged:(value) {
-				playBudget.setCategory(userController.categoryMap[category], value)
+				playBudget.setCategory(userController.categoryMap[category], value);
 			},
 			onChangeEnd:(value){
 				playBudget.setCategory(userController.categoryMap[category], value);
