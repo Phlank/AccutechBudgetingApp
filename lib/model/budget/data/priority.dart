15b068cb748ba993b3968944635c8b0c8e272056
Colin <ledbetter.colin.m@gmail.com>
2020-02-19 14:02:01 -0500

    Cleaned up priority unserialize

--- lib/model/budget/data/priority.dart
+++ lib/model/budget/data/priority.dart
@@ -12,33 +12,18 @@ class Priority implements Serializable {
       NAME_OTHER = 'Other';
 
   final String name;
+  final int value;
 
-  static const required = Priority(NAME_REQUIRED);
-  static const need = Priority(NAME_NEED);
-  static const want = Priority(NAME_WANT);
-  static const savings = Priority(NAME_SAVINGS);
-  static const other = Priority(NAME_OTHER);
+  static const required = Priority(NAME_REQUIRED, 1);
+  static const need = Priority(NAME_NEED, 2);
+  static const want = Priority(NAME_WANT, 3);
+  static const savings = Priority(NAME_SAVINGS, 4);
+  static const other = Priority(NAME_OTHER, 5);
 
-  static const List<Priority> DEFAULT_PRIORITIES = [
-    required,
-    need,
-    want,
-    savings,
-    other
-  ];
-
-  static final Map<Priority, int> _valueOf = {
-    required: 5,
-    need: 4,
-    want: 3,
-    savings: 2,
-    other: 1
-  };
-
-  const Priority(this.name);
+  const Priority(this.name, this.value);
 
   int compareTo(Priority other) {
-    return _valueOf[this].compareTo(_valueOf[other]);
+    return value.compareTo(other.value);
   }
 
   String get serialize {
@@ -49,14 +34,24 @@ class Priority implements Serializable {
 
   static Priority unserialize(dynamic input) {
     if (input is String) input = jsonDecode(input);
-    if (input[KEY_NAME] == NAME_REQUIRED) return required;
-    if (input[KEY_NAME] == NAME_NEED) return need;
-    if (input[KEY_NAME] == NAME_WANT) return want;
-    if (input[KEY_NAME] == NAME_SAVINGS) return savings;
-    return other;
+    String inputName = input[KEY_NAME];
+    switch (inputName) {
+      case NAME_REQUIRED:
+        return required;
+      case NAME_NEED:
+        return need;
+      case NAME_WANT:
+        return want;
+      case NAME_SAVINGS:
+        return savings;
+      case NAME_OTHER:
+        return other;
+      default:
+        return other;
+    }
   }
 
   bool operator ==(Object o) => o is Priority && name == o.name;
 
-  int get hashCode => name.hashCode;
+  int get hashCode => name.hashCode ^ value.hashCode;
 }
