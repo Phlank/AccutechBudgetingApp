cb193362536c832a25a33b7edcdd84aeebabe5c0
CaKellum <cakellum@bsu.edu>
2020-02-22 12:46:58 -0500

    added total edit functionality and added to the button for transactionlist

--- lib/view/pages/transactions_details.dart
+++ lib/view/pages/transactions_details.dart
@@ -2,6 +2,8 @@ import 'package:budgetflow/model/budget/category/category.dart';
 import 'package:budgetflow/model/budget/transaction/transaction.dart';
 import 'package:budgetflow/model/budget/transaction/transaction_list.dart';
 import 'package:budgetflow/view/budgeting_app.dart';
+import 'package:budgetflow/view/global_widgets/drop_downs.dart';
+import 'package:budgetflow/view/sidebar/account_display.dart';
 import 'package:budgetflow/view/utils/input_validator.dart';
 import 'package:budgetflow/view/utils/output_formatter.dart';
 import 'package:flutter/material.dart';
@@ -65,7 +67,6 @@ class TransactionDetail{
   }
 
    Transaction _mapToTrans() {
-
      return new Transaction.withTime(transactionMap['vendor'], transactionMap['method'],
          double.tryParse(transactionMap['amount']), Category.categoryFromString(transactionMap['category']), tran.time);
    }
@@ -111,13 +112,31 @@ class TransactionDetail{
   }
 
   RaisedButton _returnToList(){
-     return RaisedButton();
+     return RaisedButton(
+       child: Text('return to list'),
+       onPressed: (){
+         Navigator.pushNamed(context, AccountDisplay.ROUTE);
+       },
+     );
   }
 
    Scaffold editDetail(Transaction t,BuildContext context){
      this.tran = t;
      this.context = context;
-    return Scaffold();
+    return Scaffold(
+      body: Table(
+        children: <TableRow>[
+          _genericTextField(t.vendor, 'vendor'),
+          _genericTextField(t.amount, 'amount'),
+          TableRow(children:[Text('Category'),DropDowns().categoryDrop(Category.miscellaneous, (Category newCat){
+            transactionMap['category'] = newCat.name;
+          })],),
+          TableRow(children:[Text('Method'),DropDowns().methodDrop('cash', (String method){
+            transactionMap['method'] = method;
+          })],),
+        ],
+      ),
+    );
   }
 
    Scaffold viewDetail(Transaction t,BuildContext context){
