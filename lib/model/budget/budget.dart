c89244810898b9a163318270881f7602abcedbeb
Colin <ledbetter.colin.m@gmail.com>
2020-03-22 14:37:21 -0400

    Budget fixed

--- lib/model/budget/budget.dart
+++ lib/model/budget/budget.dart
@@ -1,13 +1,9 @@
-import 'package:budgetflow/model/budget/allocation.dart';
 import 'package:budgetflow/model/budget/allocation_list.dart';
 import 'package:budgetflow/model/budget/budget_type.dart';
 import 'package:budgetflow/model/budget/category/category.dart';
-import 'package:budgetflow/model/budget/category/category_list.dart';
 import 'package:budgetflow/model/budget/transaction/transaction.dart';
 import 'package:budgetflow/model/budget/transaction/transaction_list.dart';
 import 'package:budgetflow/model/history/month.dart';
-import 'package:budgetflow/model/util/dates.dart';
-import 'package:calendarro/date_utils.dart';
 import 'package:flutter/widgets.dart';
 
 import 'category/priority.dart';
@@ -61,7 +57,8 @@ class Budget {
 
   double setAllotment(Category category, double amount) {
     allotted
-    .
+        .get(category)
+        .value = amount;
   }
 
   void setIncome(double income) {
@@ -71,106 +68,40 @@ class Budget {
   void addTransaction(Transaction transaction) {
     transactions.add(transaction);
     if (transaction.category == null) {
-      actual[Category.uncategorized] += -transaction.amount;
+      actual
+          .get(transaction.category)
+          .value += -transaction.amount;
     }
     if (transaction.category == Category.income) {
       actualIncome += transaction.amount;
-      actual[transaction.category] += transaction.amount;
+      actual
+          .get(transaction.category)
+          .value += transaction.amount;
     } else {
-      actual[transaction.category] += -transaction.amount;
+      actual
+          .get(transaction.category)
+          .value += -transaction.amount;
     }
-  }
-
-  double get netMonth {
-    double net = 0.0;
-    transactions.forEach((t) {
-      net += t.amount;
-    });
-    return net;
-  }
-
-  double get netWeek {
-    double net = 0.0;
-    transactions.forEach((t) {
-      if (t.time.isAfter(DateTime.now().subtract(Duration(days: 7)))) {
-        net += t.amount;
-      }
-    });
-    return net;
-  }
-
-  double get balanceMonth => expectedIncome - spent;
-
-  double get balanceWeek {
-    return _getWeeklyIncome() + _getWeeklySpending();
-  }
-
-  double _getWeeklyIncome() {
-    int numDaysInWeek =
-        Dates.getEndOfWeek().difference(Dates.getStartOfWeek()).inDays;
-    return expectedIncome *
-        numDaysInWeek /
-        DateUtils.getLastDayOfCurrentMonth().day.toDouble();
-  }
-
-  double _getWeeklySpending() {
-    double weeklySpending = 0.0;
-    transactions.forEach((transaction) {
-      if (transaction.time.isBefore(Dates.getEndOfWeek()) &&
-          transaction.time.isAfter(Dates.getStartOfWeek())) {
-        if (transaction.category != Category.income)
-          weeklySpending += transaction.amount;
-      }
-    });
-    return weeklySpending;
-  }
-
-  double getAllottedPriority(Priority priority) {
-    double total = 0;
-    allotted.forEach((category, amount) {
-      if (category.priority == priority) total += amount;
-    });
-    return total;
-  }
-
-  double getActualPriority(Priority priority) {
-    double total = 0;
-    actual.forEach((category, amount) {
-      if (category.priority == priority) total += amount;
-    });
-    return total;
-  }
-
-  double getRemainingPriority(Priority priority) {
-    return getAllottedPriority(priority) - getActualPriority(priority);
-  }
-
-  double getAllottedCategory(Category category) {
-    return allotted[category];
-  }
-
-  double getActualCategory(Category category) {
-    return actual[category];
-  }
-
-  double getRemainingCategory(Category category) {
-    return allotted[category] - actual[category];
   }
 
   List<Category> getCategoriesOfPriority(Priority priority) {
     List<Category> list = List();
-    allotted.forEach((category, double) {
-//      print("Category: " + category.name + " has a priority of " +
-//          category.priority.name);
-//      print(priority.name + " == " + category.priority.name + " is " +
-//          (category.priority == priority).toString());
-      if (category.priority == priority) list.add(category);
+    allotted.forEach((allocation) {
+      if (allocation.category.priority == priority) list.add(
+          allocation.category);
     });
     return list;
   }
 
   void removeTransaction(Transaction transaction) {
     transactions.remove(transaction);
-    // TODO update income if it has category of income
+    // update income if it has category of income
+    if (transaction.category == Category.income) {
+      actualIncome -= transaction.amount;
+    }
+    // update actual of the category
+    actual
+        .get(transaction.category)
+        .value -= transaction.amount;
   }
 }
