f473b4124def070b6389494e0643b0bb9ef9ae4b
Colin <ledbetter.colin.m@gmail.com>
2020-03-24 00:54:46 -0400

    Added account and payment method

---
+++ lib/model/account/account.dart
@@ -0,0 +1,74 @@
+import 'package:budgetflow/model/account/payment_method.dart';
+import 'package:budgetflow/model/budget/transaction/transaction.dart';
+import 'package:budgetflow/model/budget/transaction/transaction_list.dart';
+import 'package:flutter/foundation.dart';
+
+class Account extends PaymentMethod {
+  String accountName;
+  TransactionList accountTransactions;
+
+  Account({
+    @required String methodName,
+    @required String accountName,
+    this.accountTransactions,
+  }) : super(methodName) {
+    if (accountTransactions == null) accountTransactions = TransactionList();
+  }
+
+  void addTransaction(Transaction t) {
+    if (!accountTransactions.contains(t)) {
+      accountTransactions.add(t);
+    }
+  }
+
+  double getSpending() {
+    if (accountTransactions == null) return 0.0;
+    double total = 0.0;
+    accountTransactions.forEach((transaction) {
+      if (transaction.category.isSpending) {
+        total += transaction.amount.abs();
+      }
+    });
+    return total;
+  }
+
+  double getSpendingRange(DateTime from, DateTime to) {
+    if (accountTransactions == null) return 0.0;
+    double total = 0.0;
+    accountTransactions.forEach((transaction) {
+      // Is the transaction a spending transaction and is it in the specified range?
+      if (transaction.category.isSpending &&
+          _transactionInRange(transaction, from, to)) {
+        total += transaction.amount.abs();
+      }
+    });
+    return total;
+  }
+
+  bool _transactionInRange(Transaction t, DateTime from, DateTime to) {
+    return t.time.isAfter(from) && t.time.isBefore(to);
+  }
+
+  double getIncrease() {
+    if (accountTransactions == null) return 0.0;
+    double total = 0.0;
+    accountTransactions.forEach((transaction) {
+      if (transaction.category.isIncome || transaction.category.isSaving) {
+        total += transaction.amount.abs();
+      }
+    });
+    return total;
+  }
+
+  double getIncreaseInRange(DateTime from, DateTime to) {
+    if (accountTransactions == null) return 0.0;
+    double total = 0.0;
+    accountTransactions.forEach((transaction) {
+      if ((transaction.category.isIncome || transaction.category.isSaving) &&
+          _transactionInRange(transaction, from, to)) {
+        total += transaction.amount.abs();
+      }
+    });
+    return total;
+  }
+}
