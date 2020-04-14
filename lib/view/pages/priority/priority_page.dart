4ad2829f6d36b45f091607948c6a2211ef40865f
CaKellum <cakellum@bsu.edu>
2020-04-14 17:57:17 -0400

    plus button added to savings

--- lib/view/pages/priority/priority_page.dart
+++ lib/view/pages/priority/priority_page.dart
@@ -6,22 +6,42 @@ import 'package:flutter/material.dart';
 
 class PriorityPage extends StatelessWidget {
   final Priority priority;
+  List <IconButton> actions = [];
 
   PriorityPage(this.priority);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+
+  if(this.priority == Priority.savings){
+    this.actions =  [
+      IconButton(
+        icon: Icon(Icons.edit),
+        onPressed: () {
+          Navigator.of(context).push(RouteUtil.routeWithSlideTransition(
+          GeneralCategory(priority.name)));
+        }),
+      IconButton(
+        icon: Icon(Icons.plus_one),
+        onPressed: (){
+          print('plus');
+        },
+      ),
+    ];
+  } else{
+    this.actions =  [
+      IconButton(
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            Navigator.of(context).push(RouteUtil.routeWithSlideTransition(
+                GeneralCategory(priority.name)));
+          }),
+    ];
+  }
     return Scaffold(
         appBar: AppBar(
           title: Text(priority.name),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {
-                  Navigator.of(context).push(RouteUtil.routeWithSlideTransition(
-                      GeneralCategory(priority.name)));
-                })
-          ],
+          actions: this.actions,
         ),
         body: CategoryListView(priority));
   }
