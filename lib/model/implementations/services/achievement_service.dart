36c9c78669b4e32a5503a968542d3081db4f24b4
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-04-19 00:41:29 -0400

    Moved preset achievements into defaults.dart
    Signed-off-by: Colin <ledbetter.colin.m@gmail.com>

--- lib/model/implementations/services/achievement_service.dart
+++ lib/model/implementations/services/achievement_service.dart
@@ -1,26 +1,32 @@
+import 'package:budgetflow/global/defaults.dart';
 import 'package:budgetflow/global/strings.dart';
 import 'package:budgetflow/model/abstract/service.dart';
+import 'package:budgetflow/model/data_types/achievement.dart';
 import 'package:budgetflow/model/data_types/achievement_list.dart';
 import 'package:budgetflow/model/implementations/services/file_service.dart';
 import 'package:budgetflow/model/implementations/services/service_dispatcher.dart';
+import 'package:budgetflow/model/utils/serializer.dart';
 
 class AchievementService implements Service {
   ServiceDispatcher _dispatcher;
   FileService _fileService;
   AchievementList _earned;
-  AchievementList _available;
+  AchievementList _possible;
 
   AchievementService(this._dispatcher) {
     _fileService = _dispatcher.getFileService();
   }
 
   Future start() async {
-    // TODO: implement start
     if (await _filesExist()) {
-      await _loadPossibleAchievements();
-      await _loadEarnedAchievements();
+      await _loadAchievements();
+    } else {
+      _earned = List();
+      _possible = List();
+      for (var achievement in defaultAchievements) {
+        _possible.add(achievement);
+      }
     }
-    // else earned is empty list, possible is default list
   }
 
   Future<bool> _filesExist() async {
@@ -33,17 +39,50 @@ class AchievementService implements Service {
     await _loadEarnedAchievements();
   }
 
-  Future _loadPossibleAchievements() {
-
+  Future _loadEarnedAchievements() async {
+    String content =
+    await _fileService.readAndDecryptFile(earnedAchievementsFilepath);
+    _earned = Serializer.unserialize(achievementListKey, content);
   }
 
-  Future _loadEarnedAchievements() {
-
+  Future _loadPossibleAchievements() async {
+    String content =
+    await _fileService.readAndDecryptFile(possibleAchievementsFilepath);
+    _earned = Serializer.unserialize(achievementListKey, content);
   }
 
   Future stop() {
-    // TODO: implement stop
-    // save the list of possible achievements
-    // save the list of earned achievements
+    save();
+  }
+
+  Future save() {
+    _saveEarnedAchievements();
+    _savePossibleAchievements();
+  }
+
+  Future _saveEarnedAchievements() {
+    String content = _earned.serialize;
+    return _fileService.encryptAndWriteFile(
+      earnedAchievementsFilepath,
+      content,
+    );
+  }
+
+  Future _savePossibleAchievements() {
+    String content = _possible.serialize;
+    return _fileService.encryptAndWriteFile(
+      possibleAchievementsFilepath,
+      content,
+    );
+  }
+
+  bool isEarned(Achievement achievement) {
+    return _earned.contains(achievement);
+  }
+
+  void earn(Achievement achievement) {
+    if (!_earned.contains(achievement)) {
+      _earned.add(achievement);
+    }
   }
 }
