32ffb1403bfd1b0412947863ff88311bc123c6c7
Colin Ledbetter <ledbetter.colin.m@gmail.com>
2020-04-24 19:06:22 -0400

    Docs
    Signed-off-by: Colin <ledbetter.colin.m@gmail.com>

--- lib/model/abstract/budget_factory.dart
+++ lib/model/abstract/budget_factory.dart
@@ -1,7 +1,9 @@
 import 'package:budgetflow/model/data_types/budget.dart';
 
 abstract class BudgetFactory {
+  /// Returns a new budget based on user-provided info.
   Budget newFromInfo();
 
+  /// Returns a new budget based on the values in an older budget.
   Budget newMonthBudget(Budget old);
 }
--- lib/model/abstract/file_io.dart
+++ lib/model/abstract/file_io.dart
@@ -1,13 +1,17 @@
-
-
+/// Interface for File IO operations.
 abstract class FileIO {
+  /// Write a file to disk at [path] with [content].
   Future writeFile(String path, String content);
 
+  /// Write a file to disk at [path] with an encrypted form of [content].
   Future encryptAndWriteFile(String path, String content);
 
+  /// Read a file from disk at [path].
   Future<String> readFile(String path);
 
+  /// Read a file from disk at [path] and decrypt it.
   Future<String> readAndDecryptFile(String path);
 
+  /// Returns whether or not a File at [path] exists.
   Future<bool> fileExists(String path);
 }
